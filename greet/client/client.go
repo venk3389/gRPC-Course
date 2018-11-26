1c2c52df201ac1674cbe7a968b3d8d7931cbf9e0
vv4435 <venkat3389@gmail.com>
2018-11-25 17:31:13 -0800

    adding a simple error handling to greet gRPC stubs

--- greet/client/client.go
+++ greet/client/client.go
@@ -8,31 +8,30 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-func doGreetWithDeadline(cc *grpc.ClientConn, first_name,last_name string){
 
-s := greetpb.NewGreetServiceClient(cc)
-        response, err := s.GreetWithDeadline(context.Background(), &greetpb.GreetWithDeadlineRequest{
-                Greeting: &greetpb.Greeting{
-                        FirstName: first_name,
-                        LastName:  last_name,
-                },
-        })
-        if err != nil {
-                errStatus, ok := status.FromError(err)
-                // lets print the error code which is `INVALID_ARGUMENT`
-                if ok {
-                        if codes.InvalidArgument == errStatus.Code() {
-                                // do your stuff here
-                                fmt.Println("Message:", errStatus.Message(), "Code:", errStatus.Code())
-                        }
-                        return
-                }
-                fmt.Println(err.Error())
-                return
-        }
-        fmt.Println("Response from server: ", response.GetResponse())
+func doGreetWithDeadline(cc *grpc.ClientConn, first_name, last_name string) {
 
-
+	s := greetpb.NewGreetServiceClient(cc)
+	response, err := s.GreetWithDeadline(context.Background(), &greetpb.GreetWithDeadlineRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: first_name,
+			LastName:  last_name,
+		},
+	})
+	if err != nil {
+		errStatus, ok := status.FromError(err)
+		// lets print the error code which is `INVALID_ARGUMENT`
+		if ok {
+			if codes.InvalidArgument == errStatus.Code() {
+				// do your stuff here
+				fmt.Println("Message:", errStatus.Message(), "Code:", errStatus.Code())
+			}
+			return
+		}
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Response from server: ", response.GetResponse())
 
 }
 func main() {
@@ -43,7 +42,7 @@ func main() {
 	}
 	defer cc.Close()
 	//greet call that returns an error message
-	doGreetWithDeadline(cc,"","jdhdcb")
+	doGreetWithDeadline(cc, "", "jdhdcb")
 	//greet call that returns a valid response
-	doGreetWithDeadline(cc,"Venkat","Tester")
+	doGreetWithDeadline(cc, "Venkat", "Tester")
 }
