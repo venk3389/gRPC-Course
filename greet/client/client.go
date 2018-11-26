aeee2e8188d4801eda498ef36b6bfce50b64de63
vv4435 <venkat3389@gmail.com>
2018-11-25 17:29:30 -0800

    adding a simple error handling to greet gRPC stubs

--- greet/client/client.go
+++ greet/client/client.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/venk3389/gRPC-Course/greet/greetpb"
 	"google.golang.org/grpc"
-	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+func doGreetWithDeadline(cc *grpc.ClientConn, first_name,last_name string){
 
+s := greetpb.NewGreetServiceClient(cc)
+        response, err := s.GreetWithDeadline(context.Background(), &greetpb.GreetWithDeadlineRequest{
+                Greeting: &greetpb.Greeting{
+                        FirstName: first_name,
+                        LastName:  last_name,
+                },
+        })
+        if err != nil {
+                errStatus, ok := status.FromError(err)
+                // lets print the error code which is `INVALID_ARGUMENT`
+                if ok {
+                        if codes.InvalidArgument == errStatus.Code() {
+                                // do your stuff here
+                                fmt.Println("Message:", errStatus.Message(), "Code:", errStatus.Code())
+                        }
+                        return
+                }
+                fmt.Println(err.Error())
+                return
+        }
+        fmt.Println("Response from server: ", response.GetResponse())
+
+
+
+}
 func main() {
-
+	fmt.Println("Running gRPC greet client.....")
 	cc, err := grpc.Dial("0.0.0.0:50051", grpc.WithInsecure())
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer cc.Close()
-
-	s := greetpb.NewGreetServiceClient(cc)
-	response, err := s.GreetWithDeadline(context.Background(), &greetpb.GreetWithDeadlineRequest{
-		Greeting: &greetpb.Greeting{
-			FirstName: "venkat",
-			LastName:  "lester",
-		},
-	})
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println("Response from server: ", response.GetResponse())
+	//greet call that returns an error message
+	doGreetWithDeadline(cc,"","jdhdcb")
+	//greet call that returns a valid response
+	doGreetWithDeadline(cc,"Venkat","Tester")
 }
