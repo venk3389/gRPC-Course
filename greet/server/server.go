1c7b58faf6f89f2f90bf0399223325005c813899
vv4435 <venkat3389@gmail.com>
2018-11-25 17:33:04 -0800

    adding a simple error handling to greet gRPC stubs

--- greet/server/server.go
+++ greet/server/server.go
@@ -5,13 +5,21 @@ import(
 	"fmt"
 	"net"
 	"github.com/venk3389/gRPC-Course/greet/greetpb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
 
 func (*server) GreetWithDeadline(ctx context.Context,req *greetpb.GreetWithDeadlineRequest) (*greetpb.GreetWithDeadlineResponse, error){
-	response := "Hello "+req.Greeting.GetFirstName()+"!"
+	name := req.Greeting.GetFirstName()
+	if len(name) >= 10 || len(name) == 0{
+		return nil, status.Errorf(codes.InvalidArgument,
+			"Length of `Name` cannot be more than 10 characters or empty")
+
+	}
+	response := "Hello "+name+"!"
 	return &greetpb.GreetWithDeadlineResponse{
 		Response:response,
 	},nil
